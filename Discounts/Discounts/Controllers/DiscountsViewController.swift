7ce2fa1632c55a8d053df6a6f2bad5a47072acd5
Eddy Rogier <eddyrogier@gmail.com>
2020-11-30 15:37:44 +0100

    test tableView

--- Discounts/Discounts/Controllers/DiscountsViewController.swift
+++ Discounts/Discounts/Controllers/DiscountsViewController.swift
@@ -5,19 +5,42 @@
 //  Created by Eddy R on 29/11/2020.
 //
 
+import Foundation
 import UIKit
 
-class DiscountsViewController: UIViewController {
+class DiscountsViewController: UIViewController, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
+    var items: [Item] = []
+    var vc: DiscountsViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("  L\(#line) [✴️\(type(of: self))  ✴️\(#function) ] ")
         // Do any additional setup after loading the view.
         self.title = "Discounts"
     }
     
-    convenience init(items: [Item]) {
-        self.init()
-        // init parameter from convenience
+    static func initItemsVC(items: [Item]) -> DiscountsViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "DiscountsViewController") as! DiscountsViewController
+        vc.items = items
+        print(items)
+        return vc
     }
+//    convenience init(items: [Item]) {
+//        self.init()
+//        print("  L\(#line) [✴️\(type(of: self))  ✴️\(#function) ] ")
+//        // init parameter from convenience
+//    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
 
--- Discounts/DiscountsViewControllerTest/DiscountsViewControllerTest.swift
+++ Discounts/DiscountsViewControllerTest/DiscountsViewControllerTest.swift
@@ -10,12 +10,37 @@ import XCTest
 
 class DiscountsViewControllerTest: XCTestCase {
 
+    var sut : DiscountsViewController!
+    override func setUp() {
+        super.setUp()
+        //*** For storyboard
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        sut = storyboard.instantiateViewController(identifier: "DiscountsViewController") as! DiscountsViewController
+//        _ = sut.view
+    }
     
     // start ViewLifeCycle
     func test_viewDidLoad_renderTitleNavigationItem() {
-        let sut = DiscountsViewController()
-        _ = sut.view // load view by call the view
+        sut = DiscountsViewController.initItemsVC(items: [])
+        _ = sut.view // call viewDidLoad
         XCTAssertEqual(sut.title, "Discounts")
+    }
+    func test_viewDidLoad_renderTableView() {
+        sut = DiscountsViewController.initItemsVC(items: [])
+        _ = sut.view // call viewDidLoad
+        XCTAssertNotNil(sut.tableView)
+    }
+    func test_viewDidLoad_renderTableView_Empty() {
+        sut = DiscountsViewController.initItemsVC(items: [])
+        _ = sut.view // call viewDidLoad
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    func test_viewDidLoad_renderTableView_OneRow() {
+        sut = DiscountsViewController.initItemsVC(items: [
+            Item(price: 0, tax: 0),
+        ])
+        _ = sut.view // call viewDidLoad
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
     }
 }
  
