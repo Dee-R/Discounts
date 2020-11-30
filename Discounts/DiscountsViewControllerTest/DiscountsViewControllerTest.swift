7b60f884531799c3a4ded506660aa3131261bf96
Eddy Rogier <eddyrogier@gmail.com>
2020-11-30 14:03:35 +0100

    Refactor project

---
+++ Discounts/DiscountsViewControllerTest/DiscountsViewControllerTest.swift
@@ -0,0 +1,20 @@
+//
+//  DiscountsViewControllerTest.swift
+//  DiscountsViewControllerTest
+//
+//  Created by Eddy R on 30/11/2020.
+//
+
+import XCTest
+@testable import Discounts
+
+class DiscountsViewControllerTest: XCTestCase {
+
+    
+    // start ViewLifeCycle
+    
+    func test_viewDidLoad_renderQuestionHeaderText() {
+        
+    }
+}
+ 
